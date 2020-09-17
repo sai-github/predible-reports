689e0a60e819b991459fbb8c72919255ef0841fb
sai-github <saiediitm@gmail.com>
2020-09-18 04:28:01 +0530

    chore(editor): add quill editor to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/**
+ * angular/angular
+ */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/**
+ * material/theme
+ */
 import { MaterialModule } from './material/material.module';
+
+/**
+ * predible/components
+ */
 import { HeaderComponent } from './components/header/header.component';
 import { LungReportComponent } from './components/lung-report/lung-report.component';
+
+/**
+ * Quill Editor
+ */
+
+import { QuillModule } from 'ngx-quill';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, LungReportComponent],
@@ -15,6 +32,7 @@ import { LungReportComponent } from './components/lung-report/lung-report.compon
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    QuillModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/lung-report/lung-report.component.ts
+++ src/app/components/lung-report/lung-report.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-lung-report',
   templateUrl: './lung-report.component.html',
