73de188c97131a539ca38a37eff6bf88e3b38faf
sai-github <saiediitm@gmail.com>
2020-09-20 15:19:36 +0530

    fix(https): modify endpoints to use http over https

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -15,7 +15,7 @@ export class UtilsService {
     borderList: string[];
   }> {
     const endPoint =
-      'http://my-json-server.typicode.com/sai-github/predible-reports-mock-data/analysisInfo';
+      'https://my-json-server.typicode.com/sai-github/predible-reports-mock-data/analysisInfo';
     return this.http
       .get<any>(endPoint)
       .pipe(map((res) => res.nodule));
